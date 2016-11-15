f0fa2f539aa748da16b9d9af1ae54bfb104a05fe
Stephen Smith <longhornsteve1@yahoo.com>
2016-11-15 13:53:52 -0600

    change strings to integers

--- ruby/hamsters.rb
+++ ruby/hamsters.rb
@@ -1,13 +1,18 @@
 puts "what is the hamster's name?"
 hamster_name = gets.chomp
 puts "enter the hamster's volume level from 1-10"
-volume_level = gets.chomp
+volume_level = gets.chomp.to_i
 puts "what is the hamster's fur color?"
 fur_color = gets.chomp
-puts "is it a good candidate for adoption? (yes/no)"
+puts "it's a good candidate for adoption? (yes/no)"
 adoptable = gets.chomp
+if adoptable == "yes"
+	adoptable = true
+else adoptable = false	
+end
+p adoptable
 puts "what is the estimated age?"
-age = gets.chomp
+age = gets.chomp.to_i
 if age == ""
 		age = nil
 end 
