f3b54c875a428fc892f7fcdf0cc7a5933d1f16de
Stephen Smith <longhornsteve1@yahoo.com>
2016-11-15 13:39:34 -0600

    adding release 2 to file

---
+++ ruby/hamsters.rb
@@ -0,0 +1,15 @@
+puts "what is the hamster's name?"
+hamster_name = gets.chomp
+puts "enter the hamster's volume level from 1-10"
+volume_level = gets.chomp
+puts "what is the hamster's fur color?"
+fur_color = gets.chomp
+puts "is it a good candidate for adoption? (yes/no)"
+adoptable = gets.chomp
+puts "what is the estimated age?"
+age = gets.chomp
+if age == ""
+		age = nil
+end 
+ 
+
