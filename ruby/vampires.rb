ee15e1d1205b5dbd2a27d3876e3e6b4464850f44
Stephen Smith <longhornsteve1@yahoo.com>
2016-11-16 14:46:01 -0600

    working on results questions

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -14,24 +14,34 @@ puts "What year were you born in?(ex: 1979)"
 	birth_year = gets.chomp.to_i
 
 current_year = 2016
-def actual_age(birth, current, age)
-	current - birth == age
+if current_year - birth_year == stated_age
+	actual_age = true
+else actual_age = false
 end
-
-actual_age(birth_year, current_year, stated_age)
 
 puts "Our company cafeteria serves garlic bread...should we order some for you?(yes/no)"
 	garlic_bread = gets.chomp
-if garlic_bread == "no"
-	employee_likes_garlic = false
-else employee_likes_garlic = true
+if garlic_bread == "yes"
+	employee_likes_garlic = true
+else employee_likes_garlic = false
 end
 
 puts "Would you like to enroll in the company's health insurance?(yes/no)"
 	health_insurance = gets.chomp
 if health_insurance == "yes"
 		employee_wants_health = true
-else employee_wants_health = false
+else employee_wants_health = false 
 end
-
+#Can't figure out how to bypass almost certain condition#
+if 	employee_def_vampire == true
+		puts "Definitely a Vampire!"
+elsif !employee_def_vampire && actual_age && employee_likes_garlic && employee_wants_health
+		puts "Probably not a Vampire."
+elsif (actual_age and (employee_likes_garlic or employee_wants_health)) == false
+		puts "Probably a Vampire."
+elsif  (employee_def_vampire && employee_name && employee_likes_garlic && employee_wants_health) == false 
+		puts "Almost Certainly A Vampire."
+else puts "Results inconclusive."
+end
+	
 
