0a0bbc9b1bf9dff59ecc5419837ef3219e4a7baf
Stephen Smith <longhornsteve1@yahoo.com>
2016-11-15 21:28:39 -0600

    adding questions to file

---
+++ ruby/vampires.rb
@@ -0,0 +1,10 @@
+puts "Hello, what is your name?"
+employee_name = gets.chomp
+puts "How old are you?"
+stated_age = gets.chomp
+puts "What year were you born in?"
+birth_year = gets.chomp
+puts "Our company cafeteria serves garlic bread...should we order some for you?"
+garlic_bread = gets.chomp
+puts "Would you like to enroll in the company's health insurance?"
+health_insurance = gets.chomp
