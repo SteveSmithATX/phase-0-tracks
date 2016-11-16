e064a79d1e5dd75ac00c54e2ea04c3acfebf0d35
Stephen Smith <longhornsteve1@yahoo.com>
2016-11-16 11:57:15 -0600

    adding if-else statements to the first question

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -1,9 +1,19 @@
-puts "Hello, what is your name?"
+puts "Hello, what is your full name?"
 employee_name = gets.chomp
+if employee_name == "Count Dracula"
+		def_vampire = true
+elsif employee_name == "Tu Fang"
+		def_vampire = true
+elsif employee_name == "Drake Cula"
+		def_vampire = true
+else def_vampire = false	
+end
 puts "How old are you?"
 stated_age = gets.chomp
 puts "What year were you born in?"
 birth_year = gets.chomp
+
+
 puts "Our company cafeteria serves garlic bread...should we order some for you?"
 garlic_bread = gets.chomp
 puts "Would you like to enroll in the company's health insurance?"
