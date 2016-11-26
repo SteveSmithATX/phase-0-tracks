94eb4489ee3f070fc495d0532e9bb1b962190473
Stephen Smith <longhornsteve1@yahoo.com>
2016-11-25 23:33:51 -0600

    working on insertion sort array

--- ruby/problem_solving_methods.rb
+++ ruby/problem_solving_methods.rb
@@ -1,18 +1,33 @@
-#arr = [69, 12,	27,	71,	5,
-#59,	19,	16,	34,	90,
-#82,	10,	80,	17,	33,
-#26,	12,	23,	4,	18,
-#94,	6,	63,	97,	29,
-#19,	64,	56,	5,	48]
 
-#def search_array(integer)
-#	if search_array.include integer
+arr = [69, 12,	27,	71,	5,
+59,	19,	16,	34,	90,
+82,	10,	80,	17,	33,
+26,	12,	23,	4,	18,
+94,	6,	63,	97,	29,
+19,	64,	56,	5,	48]
 
-#	else
-#		puts nil
-#	end
-#end
- 
+
+=begin
+
+def search_array(arr, integer)
+   
+   index = 0
+   position = []
+  
+   while index <arr.length
+      if arr[index] == integer
+         position.push(index) 
+      end
+      index += 1
+  end
+position = nil if (position.length < 1)
+position
+end 
+
+p search_array(arr, 21)
+p search_array(arr, 63)
+=end
+######################## 
 #def fib(x)
 #  fib_array = [0, 1]
 #  index = 0
@@ -35,6 +50,8 @@
 
 #fib(100)
 
+############################
+
 #pseudocode insertion sort
 # - Start at first index
 #  - evaluate that expression
@@ -46,6 +63,35 @@
 #  - if lower, move into the sorted indices, if higher, keep where it is
 #- continue to iterate through list until you reach an entire sorted array
 
+arr = [69, 12,	27,	71,	5,
+59,	19,	16,	34,	90,
+82,	10,	80,	17,	33,
+26,	12,	23,	4,	18,
+94,	6,	63,	97,	29,
+19,	64,	56,	5,	48]
 
+def Insertion_Sort(arr)
+    for size in 2..arr.length
+        element = arr[size-1]   
+
+        index = size - 2
+        while(index >= 0) && (element < arr[index])
+            arr[index + 1] = arr[index]
+            index-=1
+        end
+        arr[index+1] = element
+    end
+        
+end
+
+p Insertion_Sort(arr)
+p arr
+
+
+#until arr.length
+#		y = num_sel
+#		less_than = x-1
+##	end
+#	end
 
 
