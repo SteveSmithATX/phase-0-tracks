27418e4afb7002c69fcb3a43528c2caa65c9adde
Stephen Smith <longhornsteve1@yahoo.com>
2016-11-26 16:17:44 -0600

    creating pseudocoding

---
+++ ruby/alias_manager.rb
@@ -0,0 +1,20 @@
+####### let's create a fake name for our spy#########
+# - receive the name in a method
+# 	- downcase the name 
+# - split the name into an array of words 
+#   -"first last".split(' ')
+# - swap the first and last name
+#	- .reverse
+# - change the vowels aeiou to the next vowel in the sequence
+# 	- split the word into an array
+# 	- run method to shuffle vowel letters over
+# 	- if its an edge case
+# - change the consonants to the next consonant in the alphabet 
+# 	- run method to shuffle consonant letters over
+#  Felicia Torres
+
+def spy_name(real_name)
+
+end
+
+spy_name(Felicia Torres)
