d99733d4684ca7539cc4e48503161bcd731a3249
Stephen Smith <longhornsteve1@yahoo.com>
2016-11-28 08:35:28 -0600

    just un-commented the work

--- ruby/problem_solving_methods.rb
+++ ruby/problem_solving_methods.rb
@@ -1,54 +1,54 @@
 
-# arr = [69, 12,	27,	71,	5,
-# 59,	19,	16,	34,	90,
-# 82,	10,	80,	17,	33,
-# 26,	12,	23,	4,	18,
-# 94,	6,	63,	97,	29,
-# 19,	64,	56,	5,	48]
+arr = [69, 12,	27,	71,	5,
+59,	19,	16,	34,	90,
+82,	10,	80,	17,	33,
+26,	12,	23,	4,	18,
+94,	6,	63,	97,	29,
+19,	64,	56,	5,	48]
 
 
 
 
-# def search_array(arr, integer)
+def search_array(arr, integer)
    
-#    index = 0
-#    position = []
+   index = 0
+   position = []
   
-#    while index <arr.length
-#       if arr[index] == integer
-#          position.push(index) 
-#       end
-#       index += 1
-#   end
-# position = nil if (position.length < 1)
-# position
-# end 
+   while index <arr.length
+      if arr[index] == integer
+         position.push(index) 
+      end
+      index += 1
+  end
+position = nil if (position.length < 1)
+position
+end 
 
-# p search_array(arr, 21)
-# p search_array(arr, 63)
+p search_array(arr, 21)
+p search_array(arr, 63)
 
-# ######################## 
-# def fib(x)
-#  fib_array = [0, 1]
-#  index = 0
+######################## 
+def fib(x)
+ fib_array = [0, 1]
+ index = 0
 
-# 	while index < fib_array.length
+	while index < fib_array.length
 
-#    fib_array.push ((fib_array.last) + (fib_array[fib_array.length-2]))
+   fib_array.push ((fib_array.last) + (fib_array[fib_array.length-2]))
   
-#    index += 1
+   index += 1
     
-#   break if x == fib_array.length
+  break if x == fib_array.length
 
-#  	end
+ 	end
   
-#  p fib_array
+ p fib_array
   
-#  p fib_array.ast
+ p fib_array.last
 
-# end
+end
 
-# fib(100)
+fib(100)
 
 ############################
 
@@ -71,17 +71,17 @@ arr = [69, 12,	27,	71,	5,
 19,	64,	56,	5,	48]
 
 def Insertion_Sort(arr)
-    for size in 2..arr.length
-        element = arr[size-1]   
+    for int in 1..arr.length
+        element = arr[int-1]   
 
-        index = size - 2
+        index = int - 2
         while(index >= 0) && (element < arr[index])
             arr[index + 1] = arr[index]
             index-=1
         end
         arr[index+1] = element
     end
-        
+p arr
 end
 
 p Insertion_Sort(arr)
