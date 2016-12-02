5aaa9b5168e49f82a6e63724a21b71258a257819
Stephen Smith <longhornsteve1@yahoo.com>
2016-12-02 08:47:34 -0600

    simplify

--- ruby/alias_manager.rb
+++ ruby/alias_manager.rb
@@ -16,16 +16,27 @@
 
 
 ###########This downcases the name, splits it and reverses it.##################
-# def spy_name(real_name)
-# 	real_name.downcase.split(' ').reverse 
-# end
 
-#   spy_name("Felicia Torres")
+vowels = ["a", "e", "i", "o", "u"]
+consonants = ["b","c","d","f","g","h","j","k","l","m","n","p","q","r","s","t","v","w","x","y","z"]
+def spy_name(real_name)
+	real_name.downcase.split(' ').reverse
+	
+
+end
+
+
+
+
+
+
+
+p  spy_name("Felicia Torres")
 # # letters = "torres".split, "felicia".split('')
 # letters.class
 # letters.map! { |letter| letter.next }
 # letters
 # new_string = letters.join('')
 
-p "Felicia Torres".downcase.split(' ').reverse = letters
-p letters.split('') 
+
+
