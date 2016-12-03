4f89e21a30566496c38c117eec9a75645b45dfbc
Stephen Smith <longhornsteve1@yahoo.com>
2016-12-02 19:01:23 -0600

    still messing with it

--- ruby/alias_manager.rb
+++ ruby/alias_manager.rb
@@ -17,21 +17,48 @@
 
 ###########This downcases the name, splits it and reverses it.##################
 
-vowels = ["a", "e", "i", "o", "u"]
-consonants = ["b","c","d","f","g","h","j","k","l","m","n","p","q","r","s","t","v","w","x","y","z"]
-def spy_name(real_name)
-	real_name.downcase.split(' ').reverse
-	
+def spy_name(real_name)                   ####Still in progress...######
+new_name = real_name.downcase
+adjust = new_name.split(' ')
+first_n = adjust[0].split('')
+last_n = adjust[1].split('')
+
+	index = 0
+	until index == first_n.last
+	vowels = ["a", "e", "i", "o", "u"]
+			if first_n[index] == ["a", "e", "i", "o"]
+				first_n[index] = vowels.next
+			elsif first_n[index] == "u"
+				first_n[index] = "a"
+			elsif first_n[index] == "z"
+				first_n[index] = "b"
+			else first_n[index].next
+				first_n[index] = first_n[index].next
+		end
+		index += 1
+	end
+#don't forget to return the method
+	index = 0
+	until index == last_n.last
+	vowels = ["a", "e", "i", "o", "u"]
+			if last_n[index] == ["a", "e", "i", "o"]
+				last_n[index] = vowels.next
+			elsif last_n[index] == "u"
+				last_n[index] = "a"
+			elsif last_n[index] == "z"
+				last_n[index] = "b"
+			else last_n[index].next
+				last_n[index] = last_n[index].next
+		end
+		index += 1
+	end
+
 
 end
 
 
+spy_name("Felicia Torres")
 
-
-
-
-
-p  spy_name("Felicia Torres")
 # # letters = "torres".split, "felicia".split('')
 # letters.class
 # letters.map! { |letter| letter.next }
