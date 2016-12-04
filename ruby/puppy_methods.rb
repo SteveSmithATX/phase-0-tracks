5e69751cf1aba0aaa19a2dd8ad6b97363262e235
Stephen Smith <longhornsteve1@yahoo.com>
2016-12-04 11:40:02 -0600

    new class and loop

--- ruby/puppy_methods.rb
+++ ruby/puppy_methods.rb
@@ -1,31 +1,70 @@
-class Puppy
+# class Puppy
 
-    def speak(num)
-    	puts "Woof " * num
-    end
+#     def speak(num)
+#     	puts "Woof " * num
+#     end
 
-	def roll_over
-		puts "rolls over"
-	end
+# 	def roll_over
+# 		puts "rolls over"
+# 	end
 	
-	def dog_years(years)
-		years * 7 
-	end
+# 	def dog_years(years)
+# 		years * 7 
+# 	end
 	
-    def fetch(toy)
-       puts "I brought back the #{toy}!"
-       toy
-    end
+#     def fetch(toy)
+#        puts "I brought back the #{toy}!"
+#        toy
+#     end
     
-    def sit
-    	puts "dog obeys"
+#     def sit
+#     	puts "dog obeys"
+#     end
+
+# end
+
+# fido = Puppy.new 
+# fido.fetch("ball")
+# fido.roll_over
+# puts fido.dog_years(4)
+# fido.sit
+# puts fido.speak(2)
+
+
+class Bear
+
+    def initialize(color, food, territory)
+    @food = food
+    @color = color
+    @territory = territory
+    end
+
+    def eats
+        puts "bear is a #{@food} that eats fish, berries, etc."
+    end
+
+    def sleeps
+        puts "bear goes into hibernation in #{@territory}"
     end
 
 end
 
-fido = Puppy.new 
-fido.fetch("ball")
-fido.roll_over
-puts fido.dog_years(4)
-fido.sit
-puts fido.speak(2)
+index = 0
+until index == 50
+puts "bear = Bear.new(black, berries, u.s.)"
+index +=1 
+end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
