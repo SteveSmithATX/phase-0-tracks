49d102a7dc9f6372c0673d0f7ae615d04ce16c99
Stephen Smith <longhornsteve1@yahoo.com>
2016-12-08 10:01:47 -0600

    Refactoring for more concise code

--- ruby/gps6/my_solution.rb
+++ ruby/gps6/my_solution.rb
@@ -17,49 +17,45 @@ class VirusPredictor
   end
 # Runs speed of spread and predicted deaths methods 
   def virus_effects
-    predicted_deaths
-    speed_of_spread(@population_density, @state)
+    num = predicted_deaths
+    sos = speed_of_spread
+    puts "#{@state} will lose #{num} people in this outbreak and will spread across the state in #{sos} months.\n\n"
   end
 
   private
 
   def predicted_deaths
     # predicted deaths is solely based on population density
+    number_of_deaths =
     if @population_density >= 200
-      number_of_deaths = (@population * 0.4).floor
+      (@population * 0.4)
     elsif @population_density >= 150
-      number_of_deaths = (@population * 0.3).floor
+      (@population * 0.3)
     elsif @population_density >= 100
-      number_of_deaths = (@population * 0.2).floor
+      (@population * 0.2)
     elsif @population_density >= 50
-      number_of_deaths = (@population * 0.1).floor
+      (@population * 0.1)
     else
-      number_of_deaths = (@population * 0.05).floor
+      (@population * 0.05)
     end
-
-    print "#{@state} will lose #{number_of_deaths} people in this outbreak"
-
+    .floor
   end
-# Checks the pop density and provides a value for how long that will take
+
   def speed_of_spread #in months
     # We are still perfecting our formula here. The speed is also affected
     # by additional factors we haven't added into this functionality.
-    speed = 0.0
-
+    speed = 
     if @population_density >= 200
-      speed += 0.5
+      0.5
     elsif @population_density >= 150
-      speed += 1
+      1
     elsif @population_density >= 100
-      speed += 1.5
+      1.5
     elsif @population_density >= 50
-      speed += 2
+      2
     else
-      speed += 2.5
+      2.5
     end
-
-    puts " and will spread across the state in #{speed} months.\n\n"
-
   end
 
 end
