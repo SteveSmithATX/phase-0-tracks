79721e1f3dd2d50531cbe008dab1a029f9b3b66e
Eleanor Roosevelt <longhornsteve1@yahoo.com>
2016-12-09 14:40:28 -0600

    learning JavaScript

---
+++ js/explore.js
@@ -0,0 +1,18 @@
+// create a function that receives a string as an argument
+// type the code to reverse the string 
+// output the string 
+
+
+function reverse(s) {
+  return s.split('').reverse().join('');
+}
+
+var reverseWord = reverse(" ") 
+
+
+reverse("hello");
+
+if (1==1){
+	document.write 
+
+}
