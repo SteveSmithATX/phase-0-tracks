1eea6a1351810974d70f82d593b87592a64a8365
Stephen Smith <longhornsteve1@yahoo.com>
2016-12-12 11:14:06 -0600

    committing to move to relase 1

--- js/algos.js
+++ js/algos.js
@@ -1,7 +1,30 @@
 // Create an array with multiple values
 // create a function that takes the values of that array and figures out the length of each value
 //   for each item in the array.length = the length.  
-//   assign each .length to a value
 //   itterate through the values determinng which number is larger (spaceship operator?)
-// return the largest number's value 
+//   return the largest number's value string
 
+var phrase = ["long phrase","longest phrase","longer phrase"];
+
+// function arraySort(array) {
+// var stringLength = phrase.length;
+// for (var i = 0; i < stringLength; i++) {
+//     console.log(phrase[i].length);
+//     }
+// }
+// The Easy Way to Do it############
+// function arraySort(array) {
+// array.sort();
+// array.reverse();
+// return console.log(array[0])
+// }
+
+// function arraySort(array) {
+// var stringLength = phrase.length;
+// for (var i = 0; i < stringLength; i++) {
+//     console.log(phrase[i]);
+//     }
+// }
+
+
+arraySort(phrase)
