e463a3e54a3233621e55dbd5572f3f1cc7af001f
Stephen Smith <longhornsteve1@yahoo.com>
2016-12-12 11:34:30 -0600

    adding release 1 functions

--- js/algos.js
+++ js/algos.js
@@ -28,3 +28,19 @@ var phrase = ["long phrase","longest phrase","longer phrase"];
 
 
 arraySort(phrase)
+
+// Function to test sharing of one key-value pair
+// we want to function to return true if at least one called key-value pair is equal 
+// IF no pairs match, due to key- or value- not matching, return false
+
+var obj_1 = {name: "Stephen", age: 37};
+var obj_2 = {name: "Tyson", age: 37};
+var obj_3 = {name: "Mike", weight: 370};
+
+function matchingValue(object_x, object_y) {
+	if (object_x.name == object_y.name || object_x.age == object_y.age)
+		console.log("true");
+	else console.log("false"); 
+}
+
+matchingValue(obj_3, obj_2)
