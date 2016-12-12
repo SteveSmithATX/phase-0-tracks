236be7942cd2ae3b09e488eaf56a4330cf09487f
Stephen Smith <longhornsteve1@yahoo.com>
2016-12-12 10:27:24 -0600

    adding algos file and pseudo coding release 0

---
+++ js/algos.js
@@ -0,0 +1,7 @@
+// Create an array with multiple values
+// create a function that takes the values of that array and figures out the length of each value
+//   for each item in the array.length = the length.  
+//   assign each .length to a value
+//   itterate through the values determinng which number is larger (spaceship operator?)
+// return the largest number's value 
+
